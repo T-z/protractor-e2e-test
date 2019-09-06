7febfe359743b431f921dcfee229e9d717fe6f32
bnguenkam <brice.nguenkam@vipco.de>
2019-09-06 12:45:43 +0200

    first commit

---
+++ e2e/public/public.e2e-spec.ts
@@ -0,0 +1,15 @@
+
+import { PublicPage } from './public.po';
+
+describe('protractor-tutorial - Public page', () => {
+  let page: PublicPage;
+
+  beforeEach(() => {
+    page = new PublicPage();
+  });
+
+  it('when user browses to our app he should see the default “public” screen', () => {
+    page.navigateTo();
+    expect(page.getPageTitleText()).toEqual('Public');
+  });
+});
